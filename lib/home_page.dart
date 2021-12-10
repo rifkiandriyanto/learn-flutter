2521f0789a1abde50cc047da6081b73fb7429490
rifkiandriyanto <andriyantorifki@gmail.com>
2021-12-10 19:32:18 +0700

    basic-flutter-3.0

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:learn_flutter/theme.dart';
+import 'package:learn_flutter/widgets/chat_tile.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -6,123 +8,103 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: blueColor,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: greenColor,
+        child: const Icon(
+          Icons.add,
+          size: 28,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Image(
-                image: AssetImage('assets/img/vector1.jpeg'),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 30),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
-                        Text(
-                          'NFT Painting Abstarct',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        SizedBox(
-                          height: 8,
-                        ),
-                        Text(
-                          'By Lorem Ipsum',
-                          style: TextStyle(
-                              fontWeight: FontWeight.w300, fontSize: 12),
-                        ),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Icon(
-                          Icons.favorite,
-                          color: Colors.amber[200],
-                        ),
-                        const SizedBox(
-                          width: 8,
-                        ),
-                        const Text(
-                          '4.5',
-                          style: TextStyle(
-                              fontWeight: FontWeight.w300, fontSize: 12),
-                        ),
-                      ],
-                    )
-                  ],
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const SizedBox(height: 30),
+                const Image(
+                  image: AssetImage('assets/img/profile.png'),
+                  height: 100,
+                  width: 100,
                 ),
-              ),
-              const SizedBox(height: 30),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 30),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Column(
-                      children: [
-                        Icon(
-                          Icons.call,
-                          color: Colors.blue[700],
-                        ),
-                        const SizedBox(height: 10),
-                        const Text(
-                          'CALL',
-                          style: TextStyle(
-                              fontWeight: FontWeight.w300, fontSize: 12),
-                        ),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Icon(
-                          Icons.near_me,
-                          color: Colors.blue[700],
-                        ),
-                        const SizedBox(height: 10),
-                        const Text(
-                          'ROUTE',
-                          style: TextStyle(
-                              fontWeight: FontWeight.w300, fontSize: 12),
-                        ),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Icon(
-                          Icons.share,
-                          color: Colors.blue[700],
-                        ),
-                        const SizedBox(height: 10),
-                        const Text(
-                          'SHARE',
-                          style: TextStyle(
-                              fontWeight: FontWeight.w300, fontSize: 12),
-                        ),
-                      ],
-                    )
-                  ],
+                const SizedBox(height: 20),
+                const Text(
+                  'Fiza Innaramecca',
+                  style: TextStyle(fontSize: 20, color: whiteColor),
                 ),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 30),
-                child: Text(
-                  'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\n\nLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\n\nLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-                  style: TextStyle(
-                    fontSize: 12,
+                const SizedBox(height: 5),
+                const Text(
+                  'CEO Fiza Corporation',
+                  style: TextStyle(fontSize: 16, color: lightGreyColor),
+                ),
+                const SizedBox(height: 30),
+                Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.all(30),
+                  decoration: const BoxDecoration(
+                    color: whiteColor,
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(40),
+                    ),
                   ),
-                  textAlign: TextAlign.justify,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Friends',
+                        style: titleTextStyle,
+                      ),
+                      // const SizedBox(
+                      //   height: 16,
+                      // ),
+                      const ChatTile(
+                        imageUrl: 'assets/img/friend1.png',
+                        name: 'Siska J.',
+                        lastMessage: 'I saw it clearly and mig...',
+                        time: 'Now',
+                        unread: true,
+                      ),
+                      const ChatTile(
+                        imageUrl: 'assets/img/friend2.png',
+                        name: 'Joshua F.',
+                        lastMessage: 'Hey, how are you? Ca...',
+                        time: '12:30',
+                        unread: false,
+                      ),
+                      const SizedBox(height: 30),
+                      Text(
+                        'Groups',
+                        style: titleTextStyle,
+                      ),
+                      const ChatTile(
+                        imageUrl: 'assets/img/group1.png',
+                        name: 'Jakarta Fair',
+                        lastMessage: 'Why does everyone ca...',
+                        time: '11:30',
+                        unread: true,
+                      ),
+                      const ChatTile(
+                        imageUrl: 'assets/img/group3.png',
+                        name: 'Android',
+                        lastMessage: 'Here here we can go...',
+                        time: '13:30',
+                        unread: false,
+                      ),
+
+                      const ChatTile(
+                        imageUrl: 'assets/img/group2.png',
+                        name: 'Bentley',
+                        lastMessage: 'The car which does not...',
+                        time: '13:30',
+                        unread: false,
+                      ),
+                    ],
+                  ),
                 ),
-              )
-            ],
+              ],
+            ),
           ),
         ),
       ),
